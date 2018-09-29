8a19c8d98dce49eb5a44a5382b9fb71e7b548833
voller <voller17@mail.ru>
2018-09-29 13:03:14 +0300

    Remove files

--- authenticationUe4/Source/authenticationUe4/authenticationUe4.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "authenticationUe4.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, authenticationUe4, "authenticationUe4" );
- 
