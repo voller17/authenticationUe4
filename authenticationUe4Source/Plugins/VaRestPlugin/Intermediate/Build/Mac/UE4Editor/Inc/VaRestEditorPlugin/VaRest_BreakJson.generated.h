542caded9a318f5d0e852c827e1a1f7116ad82ca
voller <voller17@mail.ru>
2018-09-30 11:20:55 +0300

    add varest

---
+++ authenticationUe4Source/Plugins/VaRestPlugin/Intermediate/Build/Mac/UE4Editor/Inc/VaRestEditorPlugin/VaRest_BreakJson.generated.h
@@ -0,0 +1,170 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VARESTEDITORPLUGIN_VaRest_BreakJson_generated_h
+#error "VaRest_BreakJson.generated.h already included, missing '#pragma once' in VaRest_BreakJson.h"
+#endif
+#define VARESTEDITORPLUGIN_VaRest_BreakJson_generated_h
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_30_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FVaRest_NamedType_Statics; \
+	VARESTEDITORPLUGIN_API static class UScriptStruct* StaticStruct();
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_RPC_WRAPPERS
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_RPC_WRAPPERS_NO_PURE_DECLS
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUVaRest_MakeJson(); \
+	friend struct Z_Construct_UClass_UVaRest_MakeJson_Statics; \
+public: \
+	DECLARE_CLASS(UVaRest_MakeJson, UK2Node, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VaRestEditorPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVaRest_MakeJson)
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_INCLASS \
+private: \
+	static void StaticRegisterNativesUVaRest_MakeJson(); \
+	friend struct Z_Construct_UClass_UVaRest_MakeJson_Statics; \
+public: \
+	DECLARE_CLASS(UVaRest_MakeJson, UK2Node, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VaRestEditorPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVaRest_MakeJson)
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVaRest_MakeJson(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVaRest_MakeJson) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVaRest_MakeJson); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVaRest_MakeJson); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVaRest_MakeJson(UVaRest_MakeJson&&); \
+	NO_API UVaRest_MakeJson(const UVaRest_MakeJson&); \
+public:
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVaRest_MakeJson(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVaRest_MakeJson(UVaRest_MakeJson&&); \
+	NO_API UVaRest_MakeJson(const UVaRest_MakeJson&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVaRest_MakeJson); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVaRest_MakeJson); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVaRest_MakeJson)
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_PRIVATE_PROPERTY_OFFSET
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_42_PROLOG
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_PRIVATE_PROPERTY_OFFSET \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_RPC_WRAPPERS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_INCLASS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_PRIVATE_PROPERTY_OFFSET \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_RPC_WRAPPERS_NO_PURE_DECLS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_INCLASS_NO_PURE_DECLS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_45_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class VaRest_MakeJson."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_RPC_WRAPPERS
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_RPC_WRAPPERS_NO_PURE_DECLS
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUVaRest_BreakJson(); \
+	friend struct Z_Construct_UClass_UVaRest_BreakJson_Statics; \
+public: \
+	DECLARE_CLASS(UVaRest_BreakJson, UK2Node, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VaRestEditorPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVaRest_BreakJson)
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_INCLASS \
+private: \
+	static void StaticRegisterNativesUVaRest_BreakJson(); \
+	friend struct Z_Construct_UClass_UVaRest_BreakJson_Statics; \
+public: \
+	DECLARE_CLASS(UVaRest_BreakJson, UK2Node, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VaRestEditorPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UVaRest_BreakJson)
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVaRest_BreakJson(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVaRest_BreakJson) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVaRest_BreakJson); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVaRest_BreakJson); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVaRest_BreakJson(UVaRest_BreakJson&&); \
+	NO_API UVaRest_BreakJson(const UVaRest_BreakJson&); \
+public:
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UVaRest_BreakJson(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UVaRest_BreakJson(UVaRest_BreakJson&&); \
+	NO_API UVaRest_BreakJson(const UVaRest_BreakJson&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UVaRest_BreakJson); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UVaRest_BreakJson); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UVaRest_BreakJson)
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_PRIVATE_PROPERTY_OFFSET
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_73_PROLOG
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_PRIVATE_PROPERTY_OFFSET \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_RPC_WRAPPERS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_INCLASS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_PRIVATE_PROPERTY_OFFSET \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_RPC_WRAPPERS_NO_PURE_DECLS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_INCLASS_NO_PURE_DECLS \
+	Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h_76_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class VaRest_BreakJson."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Build___Portal_Dev_Marketplace_Full_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_VaRestPlugin_Source_VaRestEditorPlugin_Classes_VaRest_BreakJson_h
+
+
+#define FOREACH_ENUM_EVAREST_JSONTYPE(op) \
+	op(EVaRest_JsonType::JSON_Bool) \
+	op(EVaRest_JsonType::JSON_Number) \
+	op(EVaRest_JsonType::JSON_String) \
+	op(EVaRest_JsonType::JSON_Object) 
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
